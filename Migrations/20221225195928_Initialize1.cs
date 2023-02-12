fef09c2d7853ea933596aba0859b26861fefdf5d
bernaoui-hamza <BERNAOUI_HAMZA@emsi-edu.ma>
2023-02-13 00:24:31 +0100

    commit project1

---
+++ Migrations/20221225195928_Initialize1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CURDOprationsDimo.Migrations
+{
+    public partial class Initialize1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUser",
+                schema: "HR",
+                table: "Employees",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUser",
+                schema: "HR",
+                table: "Employees",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+        }
+    }
+}
